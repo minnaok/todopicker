a1e069349992d1789e43cd49e06f7de082837d7c
minnaok <minnoksa@gmail.com>
2020-11-29 19:04:38 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React, { useState }  from 'react';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  DatePicker,
+  MuiPickersUtilsProvider
+} from '@material-ui/pickers';
 
+
+ 
 function App() {
+  const [date, setDate] = useState(new Date());
+  const [todo, setTodo] = useState({desc: '', date: new Date()});
+  const [todos, setTodos] = useState([]);
+  
+  
+
+  
+ 
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+    console.log(todos);
+   
+  }
+ 
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name]: event.target.value});
+    
+}
+
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header name="Todos">
+        <h1>TodoList</h1>
       </header>
+      
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <DatePicker value={date} format={"dd-MM-yyyy"} onChange={date => setDate(date)}/>  
+    </MuiPickersUtilsProvider>
+    <TextField label= "Description"type="text" name="desc" value={todo.desc} onChange={inputChanged}/>
+      <Button size= "small" variant="contained" color="secondary" onClick={addTodo}>Add</Button>
+      
+      <table><tbody>
+      {
+      todos.map((todo, index) => 
+        <tr key={index}>
+          <td>{date.toLocaleDateString()}</td>
+          <td>{todo.desc}</td>
+        
+    
+        </tr>)
+      }
+      </tbody></table>
     </div>
   );
 }
-
+ 
 export default App;
